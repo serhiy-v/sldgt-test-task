a4859dfb240f6124a380743513e92adb840adc7e
serhiy-v <sergozhulistuj@gmail.com>
2024-03-05 00:56:05 +0200

    deleted commited by mistake

--- cmd/main.go
+++ cmd/main.go
@@ -13,30 +13,4 @@ func main() {
 	srv := server.NewServer(router, verifySvc)
 
 	srv.RunServer()
-
-	//var luhn int
-	//
-	//number := 4441114469726432
-	//
-	//for i := 1; number > 0; i++ {
-	//	fmt.Println(number)
-	//	cur := number % 10
-	//	altern := false
-	//	fmt.Println("----")
-	//	fmt.Println(cur)
-	//
-	//	if i%2 == 0 {
-	//		cur = cur * 2
-	//		if cur > 9 {
-	//			cur -= 9
-	//		}
-	//	}
-	//
-	//	luhn += cur
-	//	fmt.Println("LUHHHN")
-	//	fmt.Println(luhn)
-	//	number = number / 10
-	//	altern = !altern
-	//}
-
 }
